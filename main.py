8f9e87105361a66f804b6c6f7ab7b96cbf422bb7
mikhailouski_n <jabber_mcn@tut.by>
2023-12-06 13:03:45 +0300

    Пофиксил отваливание приложения изза большого количества запросов, добавил описание

--- main.py
+++ main.py
@@ -7,27 +7,31 @@ from src.hh.api import HHApi
 
 
 async def foo():
+    """Функция для поиска вакансий."""
     try:
         hh = HHApi()
         while True:
             await hh.vacancies_list()
             logging.debug("Ищем подходящие вакансии...")
+            await asyncio.sleep(15)
     except Exception as e:
-        logging.error(f'Ошибка функции foo(): {e}')
+        logging.error(f'Ошибка функции foo(): {e}', exc_info=True)
 
 
 async def main() -> None:
+    """Основная функция для запуска бота и поиска вакансий."""
     try:
         while True:
             asyncio.create_task(dp.start_polling(bot))
             await foo()
     except Exception as e:
-        logging.error(f'Ошибка функции main(): {e}')
+        logging.error(f'Ошибка функции main(): {e}', exc_info=True)
 
 
 if __name__ == "__main__":
     try:
-        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
         asyncio.run(main())
     except Exception as e:
-        logging.error(f'Ошибка запуска бота: {e}')
+        logging.error(f'Ошибка запуска бота: {e}', exc_info=True)
